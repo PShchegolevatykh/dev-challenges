2cb8fc929ad69a21bdc6dc4e4c7464ba4cf3732e
Roman Kovalenko <quadreff.rk@gmail.com>
2022-06-12 12:36:54 +0300

    Add CircleOfChainedWords.cs

---
+++ rkovalenko/CircleOfChainedWords.cs
@@ -0,0 +1,29 @@
+﻿namespace TestConsoleApp
+{
+    public static class CircleOfChainedWords
+    {
+        public static bool IsLooped(IEnumerable<string> listOfWords)
+        {
+            var dict = new Dictionary<char, int>(listOfWords.Count() * 2);
+            foreach (var word in listOfWords)
+            {
+                dict.AddOrIncrementValue(word[0]);
+                dict.AddOrIncrementValue(word[^1]);
+            }
+
+            return dict.All(pair => pair.Value == 2);
+        }
+
+        private static void AddOrIncrementValue(this Dictionary<char, int> dictionary, char key)
+        {
+            if (dictionary.ContainsKey(key))
+            {
+                ++dictionary[key];
+            }
+            else
+            {
+                dictionary.Add(key, 1);
+            }
+        }
+    }
+}
