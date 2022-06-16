18c8d59a8cbf55fde8ac5bc2d85b3bedae76d24b
Roman Kovalenko <quadreff.rk@gmail.com>
2022-06-16 10:02:51 +0300

    Update CircleOfChainedWords.cs

--- rkovalenko/CircleOfChainedWords.cs
+++ rkovalenko/CircleOfChainedWords.cs
@@ -2,29 +2,46 @@
 {
     public static class CircleOfChainedWords
     {
-        public static bool IsLooped(IEnumerable<string> listOfWords)
+        public static bool IsLooped(List<string> listOfWords)
         {
-            var dict = new Dictionary<char, int>(listOfWords.Count() * 2);
-            foreach (var word in listOfWords)
+            if(!listOfWords.Any())
             {
-                dict.AddOrIncrementValue(word[0]);
-                dict.AddOrIncrementValue(word[^1]);
-
+                return false;
             }
 
-            return dict.All(pair => pair.Value % 2 == 0);
-        }
+            var startingWordIndex = 0;
+            var count = listOfWords.Count;
+            for(var i = 1; i < count;)
+            {
+                // compare last char of one word and the first char of the second word
+                if (listOfWords[startingWordIndex][^1] == listOfWords[i][0])
+                {
+                    if(i - startingWordIndex > 1)
+                    {
+                        var overlappingIndex = (startingWordIndex + 1) % count;
+                        (listOfWords[i], listOfWords[overlappingIndex]) = (listOfWords[overlappingIndex], listOfWords[i]);
+                    }
 
-        private static void AddOrIncrementValue(this Dictionary<char, int> dictionary, char key)
-        {
-            if (dictionary.ContainsKey(key))
-            {
-                ++dictionary[key];
+                    ++startingWordIndex;
+
+                    i = startingWordIndex + 1;
+                }
+                else
+                {
+                    ++i;
+                }
             }
-            else
+
+            for(var i = 0; i < count; ++i)
             {
-                dictionary.Add(key, 1);
+                var overlappingIndex = (i + 1) % count;
+                if(listOfWords[i][^1] != listOfWords[overlappingIndex][0])
+                {
+                    return false;
+                }
             }
+
+            return true;
         }
     }
 }
